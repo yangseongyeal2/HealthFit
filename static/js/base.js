1de179645451eb74fd9b63783205b41b5ca4d4fb
Changeon Park <qkrcksrjs@gmail.com>
2021-02-15 19:24:10 +0900

    rightbar_born

--- static/js/base.js
+++ static/js/base.js
@@ -1,3 +1,4 @@
+//hide or show subMenu
 window.onscroll = function() {fixed()};
 
 const subMenu = document.getElementById(`headbar__sub`);
@@ -11,3 +12,13 @@ function fixed() {
     subMenu.classList.remove(`off`);
   }
 }
+
+//go to top
+
+document.getElementById(`go-top`).addEventListener(`click`, () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
+})
