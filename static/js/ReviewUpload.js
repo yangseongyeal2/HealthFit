95c4c1add3c1ea257219a90adaeb8c26bb256bda
yangseongyeal2 <61301184+yangseongyeal@users.noreply.github.com>
2021-02-18 22:03:11 +0900

    fuck

--- static/js/ReviewUpload.js
+++ static/js/ReviewUpload.js
@@ -1,4 +1,24 @@
+
+
+
+
+
 function ReviewUpload(){
     alert("리뷰실행")
-    
+    var storage= firebase.storage();
+    var file=document.getElementById("file-input-img").files[0];
+    var storageRef=storage.ref();
+    var thisref=storageRef.child(file.name).put(file);
+    thisref.on('state_changed',function(snapshot){
+        console.log("file Uploaded successfully");
+    },
+    function(error){
+        console.log("error")
+    },
+    function(){
+        var downloadURL=thisref.snapshot.downloadURL;
+        console.log("got url")
+        
+    });
+
 }
