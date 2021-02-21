1b8fbc66e5f3f4b7674dbc1bfd6cbe264a8a51ec
yangseongyeal2 <61301184+yangseongyeal@users.noreply.github.com>
2021-02-21 20:48:14 +0900

    img list

--- static/js/ReviewUpload.js
+++ static/js/ReviewUpload.js
@@ -5,59 +5,15 @@
 
 
 function ReviewUpload(){
-    alert("리뷰실행")
-//     var storage= firebase.storage();
-//     var file=document.getElementById("file-input-img").files[0];
-//     var storageRef=storage.ref();
-//     var thisref=storageRef.child("file.name").put(file);
-//     var imgurl="";
+  
 
-//     thisref.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-//     function(snapshot) {
-//     // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-//     var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-//     console.log('Upload is ' + progress + '% done');
-//     switch (snapshot.state) {
-//       case firebase.storage.TaskState.PAUSED: // or 'paused'
-//         console.log('Upload is paused');
-//         break;
-//       case firebase.storage.TaskState.RUNNING: // or 'running'
-//         console.log('Upload is running');
-//         break;
-//         }
-//     }, function(error) {
-
-//     // A full list of error codes is available at
-//   // https://firebase.google.com/docs/storage/web/handle-errors
-//     switch (error.code) {
-//     case 'storage/unauthorized':
-//       // User doesn't have permission to access the object
-//     break;
-
-//     case 'storage/canceled':
-//       // User canceled the upload
-//       break;
-//     case 'storage/unknown':
-//       // Unknown error occurred, inspect error.serverResponse
-//       break;
-//   }
-//     }, function() {
-//   // Upload completed successfully, now we can get the download URL
-//     thisref.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-//     console.log('File available at', downloadURL);
-//     imgurl=downloadURL
-    
-
-//     });
-//     });
-
-
-    //delivery 정보 파이어스토어에서 가죠오기
     var documentId=document.getElementById('delivery_uid').value;
     var starPoint=document.getElementById('starPoint').value;
     var text=document.getElementById('review_textarea').value;
     var docRef = db.collection("delivery").doc(documentId);
-    var imgurl=document.getElementById('downloadURL').value;
+    //var imgurl=document.getElementById('downloadURL10').value;
+    //var imglist=document.getElementsByName('downloadURL[]').value;
+   // console.log("이미지리스트:"+imgurl)
   
     docRef.get().then(function(doc) {
         if (doc.exists) {
@@ -73,11 +29,11 @@ function ReviewUpload(){
                 ColorPoint: "",
                 ThickPoint: "",
                 timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-                downloadUrl:imgurl
+                downloadUrl:download_url_lis
             })
             .then(() => {
                 console.log("Document written with ID: ");
-                history.back();
+                //history.back();
             })
             .catch((error) => {
                 console.error("Error adding document: ", error);
--- static/js/inputimg.js
+++ static/js/inputimg.js
@@ -42,17 +42,20 @@
 // $('.preview-edit').click( function() {
 // $("#file").click();
 // } );
+let download_url_lis=[];
 
 function readInputFile(e){
+  
   let sel_files = [];
   
   sel_files = [];
+  
   $('#imgPreview').empty();
   
   let files = e.target.files;
   let fileArr = Array.prototype.slice.call(files);
   let index = 0;
-  
+  let i=0
   fileArr.forEach(function(f){
     console.log(`2`);
     if(!f.type.match("image/.*")){
@@ -61,6 +64,7 @@ function readInputFile(e){
       };
     if(files.length < 11){
         console.log(`3`);
+        console.log("테스트"+files[0]);
         sel_files.push(f);
         let reader = new FileReader();
         reader.onload = function(e){
@@ -76,52 +80,76 @@ function readInputFile(e){
 
         //스토리지에 업로드
       let storage= firebase.storage();
-      let file=document.getElementById("file-input-img").files[0];
+      //let file=document.getElementById("file-input-img").files[0];
+      let files_yang=document.getElementById("file-input-img").files;
+      let delivery_uid=document.getElementById('delivery_uid').value
+      let user_uid=document.getElementById('user_uid').value
+      let filename=delivery_uid+user_uid
+      console.log("files:"+files);
       let storageRef=storage.ref();
-      let thisref=storageRef.child("file.name").put(file);
+
       
+        filename=filename+String(i)
+        
 
-      thisref.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
-      function(snapshot) {
-    // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-      console.log('Upload is ' + progress + '% done');
-      switch (snapshot.state) {
-      case firebase.storage.TaskState.PAUSED: // or 'paused'
-        console.log('Upload is paused');
+        let thisref=storageRef.child(filename).put(files[i]);
+
+        thisref.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+        function(snapshot) {
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log('Upload is paused');
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log('Upload is running');
+          break;
+          }
+        }, function(error) {
+  
+      // A full list of error codes is available at
+    // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+        case 'storage/unauthorized':
+        // User doesn't have permission to access the object
         break;
-      case firebase.storage.TaskState.RUNNING: // or 'running'
-        console.log('Upload is running');
+  
+        case 'storage/canceled':
+        // User canceled the upload
         break;
-        }
-      }, function(error) {
-
-    // A full list of error codes is available at
-  // https://firebase.google.com/docs/storage/web/handle-errors
-      switch (error.code) {
-      case 'storage/unauthorized':
-      // User doesn't have permission to access the object
-      break;
-
-      case 'storage/canceled':
-      // User canceled the upload
-      break;
-      case 'storage/unknown':
-      // Unknown error occurred, inspect error.serverResponse
-      break;
-    }
-    }, function() {
-  // Upload completed successfully, now we can get the download URL
-    thisref.snapshot.ref.getDownloadURL().then(function(downloadURL) {
-    console.log('File available at', downloadURL);
-    document.getElementById('downloadURL').value=downloadURL
-    
-    
-
-    });
-    });
+        case 'storage/unknown':
+        // Unknown error occurred, inspect error.serverResponse
+        break;
       }
-    })
+      }, function() {
+    // Upload completed successfully, now we can get the download URL
+      thisref.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+      i++;
+      console.log('File available at', downloadURL);
+      //document.getElementById('downloadURL[]').value+=downloadURL
+      // let downloadURL_reviewindex=document.getElementById('downloadURL')
+      // downloadURL_reviewindex.innerHTML +=`
+      // <input type="hidden" id="downloadURL${i}" name="downloadURL[] value="${downloadURL}">
+      // `;
+      download_url_lis.push(downloadURL);
+
+      
+      
+      
+      
+  
+      });
+      });//thisref.on 끝
+
+
+    
+      }//if문 끝
+    })//for each문 끝
+    
+   
+    
     if(files.length > 11){
     alert("최대 10장까지 업로드 할 수 있습니다.");
     };
